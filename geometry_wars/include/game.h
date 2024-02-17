0badf02bc77b3aa10ecac884c8d110aaf8dd5e55
vijay <vjblacklight@gmail.com>
2024-02-17 17:31:09 +0530

    ADD - imgui set-up

--- geometry_wars/include/game.h
+++ geometry_wars/include/game.h
@@ -2,11 +2,17 @@
 #define gameh
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <utility>
+#include <memory>
+#include "imgui.h"
+#include "imgui-SFML.h"
 
 class Game
 {
+    sf::RenderWindow window;
 
 public:
+    Game();
     void run();
 };
 
--- geometry_wars/src/game.cpp
+++ geometry_wars/src/game.cpp
@@ -1,7 +1,22 @@
 #include <game.h>
-
+Game::Game()
+{
+    window.create(sf::VideoMode(700, 500), "Sfml test");
+}
 void Game::run()
 {
     std::cout << "hello world" << std::endl;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
+
     std::cin.get();
 }
